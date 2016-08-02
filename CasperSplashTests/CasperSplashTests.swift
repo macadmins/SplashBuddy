2d51cc42769e4cb0202010f45f169e2f2be95770
François Levaux-Tiffreau <fti@me.com>
2016-08-02 15:08:07 +0200

    initial test

--- CasperSplashTests/CasperSplashTests.swift
+++ CasperSplashTests/CasperSplashTests.swift
@@ -24,13 +24,9 @@ class CasperSplashTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(true, "just testing")
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
+
     
 }
