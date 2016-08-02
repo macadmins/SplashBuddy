6ad21424ad0d8634d53f65085d0d692c4495a121
François Levaux-Tiffreau <fti@me.com>
2016-08-02 15:01:24 +0200

    Initial Commit

---
+++ CasperSplash/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CasperSplash
+//
+//  Created by testpilotfinal on 02/08/16.
+//  Copyright © 2016 François Levaux-Tiffreau. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
