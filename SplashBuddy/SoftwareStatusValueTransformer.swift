acf52378a9ec8416f9bda6deba981c694734e3f1
Francois 'ftiff' Levaux-Tiffreau <fti@me.com>
2017-03-13 05:20:36 +0100

    Grand renaming to SplashBuddy

---
+++ SplashBuddy/SoftwareStatusValueTransformer.swift
@@ -0,0 +1,46 @@
+//
+//  SoftwareStatusValueTransformer.swift
+//  SplashBuddy
+//
+//  Created by ftiff on 05/08/16.
+//  Copyright © 2016 François Levaux-Tiffreau. All rights reserved.
+//
+
+import Cocoa
+
+class SoftwareStatusValueTransformer: ValueTransformer {
+    
+    override class func transformedValueClass() -> AnyClass {
+        return NSImage.self
+    }
+    
+    override class func allowsReverseTransformation() -> Bool {
+        return false
+    }
+    
+    override func transformedValue(_ value: Any?) -> Any? {
+        
+        guard let value = value as? Int else {
+            return nil
+        }
+        
+        switch value {
+            
+        case Software.SoftwareStatus.failed.rawValue:
+            return NSImage(named: "NSStatusUnavailable")
+            
+        case Software.SoftwareStatus.installing.rawValue:
+            return NSImage(named: "NSStatusPartiallyAvailable")
+            
+        case Software.SoftwareStatus.pending.rawValue:
+            return NSImage(named: "NSStatusNone")
+            
+        case Software.SoftwareStatus.success.rawValue:
+            return NSImage(named: "NSStatusAvailable")
+            
+        default:
+            return NSImage(named: "NSStatusNone")
+            
+        }
+    }
+}
